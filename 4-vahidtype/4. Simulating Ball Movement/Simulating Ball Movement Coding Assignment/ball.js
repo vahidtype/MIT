40f87258788062ec676dcd9f8bd595118815250a
vahidtype <78255484+vahidtype@users.noreply.github.com>
2021-10-11 15:47:03 +0530

    Add files via upload

---
+++ 4-vahidtype/4. Simulating Ball Movement/Simulating Ball Movement Coding Assignment/ball.js
@@ -0,0 +1,41 @@
+//Set global variable that would contain the position, velocity and the html element "ball"
+var positionX = 0;
+var positionY = 0;
+var velocity = 100;
+var reverse = false;
+var ball = document.getElementById("ball");
+
+//write a function that can change the position of the html element "ball"
+function moveBall() {
+  // two fixed x-axis coordinates (you will use these variable in step 3)
+  var Xmin = 0;
+  var Xmax = 300;
+
+  // two y-axis coordinates
+  var Ymin = 0;
+  var Ymax = 300;
+
+  if (reverse) {
+    positionX = positionX - velocity;
+    positionY = positionY - velocity;
+    ball.style.top = positionX + "px";
+    ball.style.left = positionY + "px";
+  } else {
+    positionX = positionX + velocity;
+    positionY = positionY + velocity;
+    ball.style.top = positionX + "px";
+    ball.style.left = positionY + "px";
+  }
+
+  if (
+    positionX > Xmax ||
+    positionX === Xmin ||
+    positionY > Ymax ||
+    positionY === Ymin
+  ) {
+    reverse = !reverse;
+  }
+}
+
+// This call the moveBall function every 100ms
+setInterval(moveBall, 100);
